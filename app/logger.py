5b8470948db47230a9f4dc5162d33008c6685830
Alen Subasic <alensubasic@gmail.com>
2025-03-19 15:09:27 -0500

    fix: conditionally add logging handlers based on DEBUG environment variable

--- app/logger.py
+++ app/logger.py
@@ -5,16 +5,18 @@ import os
 logger = logging.getLogger("music_watchdog")
 logger.setLevel(logging.DEBUG)
 
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+debug = os.getenv("DEBUG", "false").lower() == "true"
+if debug:
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
 
-# Additional file handler for persistent logs
-log_file = os.path.join(os.path.dirname(__file__), "music_watchdog.log")
-fh = logging.FileHandler(log_file)
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
+    # Additional file handler for persistent logs
+    log_file = os.path.join(os.path.dirname(__file__), "music_watchdog.log")
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
